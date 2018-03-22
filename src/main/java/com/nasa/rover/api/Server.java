d2cdc30053d2ea719565f1405ac6152974f744ef
Marcos Escribano <marcoses83@gmail.com>
2018-03-22 23:36:05 +0100

    Added endpoint to receive command sequence

--- src/main/java/com/nasa/rover/api/Server.java
+++ src/main/java/com/nasa/rover/api/Server.java
@@ -2,6 +2,10 @@ package com.nasa.rover.api;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Launcher;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class Server extends AbstractVerticle {
     public static void main(String[] args) {
@@ -10,10 +14,39 @@ public class Server extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
-        }).listen(8080);
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.get("/commands/:commandSequence").handler(this::handleGetProduct);
+
+        vertx.createHttpServer()
+//                .requestHandler(req -> {
+//                    req.response()
+//                            .putHeader("content-type", "text/plain")
+//                            .end("Hello from Vert.x!");
+//                })
+                .requestHandler(router::accept)
+                .listen(8080);
+    }
+
+    private void handleGetProduct(RoutingContext routingContext) {
+        String commandSequence = routingContext.request().getParam("commandSequence");
+        HttpServerResponse response = routingContext.response();
+        if (commandSequence == null) {
+            sendError(400, response);
+        } else {
+            //TODO: parse commands
+            String commandList = commandSequence;
+
+            if (commandList == null) {
+                sendError(404, response);
+            } else {
+                response.putHeader("content-type", "application/json").setStatusCode(200).end(commandList);
+            }
+        }
+    }
+
+    private void sendError(int statusCode, HttpServerResponse response) {
+        response.setStatusCode(statusCode).end();
     }
 }
