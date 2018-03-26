3561ebf6fe959f242b3aa8f288c27b3366e78189
Marcos Escribano <marcoses83@gmail.com>
2018-03-26 23:24:39 +0200

    Added tests for Rover class

--- src/test/java/com/nasa/rover/test/unit/model/RoverTest.java
+++ src/test/java/com/nasa/rover/test/unit/model/RoverTest.java
@@ -5,67 +5,42 @@ import com.nasa.rover.model.Position;
 import com.nasa.rover.model.Rover;
 import com.nasa.rover.model.enums.CardinalPoint;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RoverTest {
     @Mock
     private INavigator navigator;
 
-    //TODO Write tests for Rover class
-    /*@Test
-    public void moveShouldSetRoverAtExpectedPositionWhenOrientedNorthAndMovedForward() {
-        Rover rover = new Rover(new Position(2, 1), CardinalPoint.NORTH, navigator);
-        Rover expectedRover = new Rover(new Position(2, 2), CardinalPoint.NORTH, navigator);
-        String movements = "F";
+    @Test
+    public void moveShouldCallNavigator() {
+        String movements = "FLBRFLBL";
 
-        rover.move(movements);
+        new Rover(new Position(0, 0), CardinalPoint.NORTH, navigator).move(movements);
 
-        assertEquals(expectedRover, rover);
+        verify(navigator, times(2)).moveForward(any(), any());
+        verify(navigator, times(2)).moveBackward(any(), any());
+        verify(navigator, times(3)).rotateLeft(any());
+        verify(navigator, times(1)).rotateRight(any());
     }
 
     @Test
-    public void moveShouldSetRoverAtExpectedPositionWhenOrientedNorthAndMovedBackward() {
-        Rover rover = new Rover(new Position(2, 1), CardinalPoint.NORTH, navigator);
-        Rover expectedRover = new Rover(new Position(2, 0), CardinalPoint.NORTH, navigator);
-        String movements = "B";
+    public void moveShouldNotCallNavigatorWhenNoMovement() {
+        String movements = "";
 
-        rover.move(movements);
+        new Rover(new Position(0, 0), CardinalPoint.NORTH, navigator).move(movements);
 
-        assertEquals(expectedRover, rover);
+        verify(navigator, never()).moveForward(any(), any());
+        verify(navigator, never()).moveBackward(any(), any());
+        verify(navigator, never()).rotateLeft(any());
+        verify(navigator, never()).rotateRight(any());
     }
-
-    @Test
-    public void moveShouldSetRoverAtExpectedPositionWhenOrientedNorthAndTurnedLeft() {
-        Rover rover = new Rover(new Position(2, 1), CardinalPoint.NORTH, navigator);
-        Rover expectedRover = new Rover(new Position(2, 1), CardinalPoint.WEST, navigator);
-        String movements = "L";
-
-        rover.move(movements);
-
-        assertEquals(expectedRover, rover);
-    }
-
-    @Test
-    public void moveShouldSetRoverAtExpectedPositionWhenOrientedNorthAndTurnedRight() {
-        Rover rover = new Rover(new Position(2, 1), CardinalPoint.NORTH, navigator);
-        Rover expectedRover = new Rover(new Position(2, 1), CardinalPoint.EAST, navigator);
-        String movements = "R";
-
-        rover.move(movements);
-
-        assertEquals(expectedRover, rover);
-    }
-
-    @Test
-    public void moveShouldSetRoverAtExpectedPosition() {
-        Rover rover = new Rover(new Position(2, 1), CardinalPoint.NORTH, navigator);
-        Rover expectedRover = new Rover(new Position(4, 5), CardinalPoint.SOUTH, navigator);
-        String movements = "FFRFLFRBFFRB";
-
-        rover.move(movements);
-
-        assertEquals(expectedRover, rover);
-    }*/
 }
