c4b2b372253d385c4f10b3ab9d5c4a853373f0f1
Marcos Escribano <marcoses83@gmail.com>
2018-03-27 02:45:29 +0200

    Added RestClient

---
+++ src/main/java/com/nasa/rover/client/RestClient.java
@@ -0,0 +1,39 @@
+package com.nasa.rover.client;
+
+import com.nasa.rover.model.Rover;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class RestClient {
+    private Client client = ClientBuilder.newClient();
+    private String baseUrl;
+
+    public RestClient(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public Response postCommandSequence(String commandSequence) {
+        return client
+                .target(baseUrl + "/rover/command/sequence")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(commandSequence, MediaType.TEXT_PLAIN));
+    }
+
+    public Response postCommand(String command) {
+        return client
+                .target(baseUrl + "/rover/command")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(command, MediaType.TEXT_PLAIN));
+    }
+
+    public Rover getRover() {
+        return client
+                .target(baseUrl + "/rover")
+                .request(MediaType.APPLICATION_JSON)
+                .get(Rover.class);
+    }
+}
